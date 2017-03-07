fada2f69516a312259412c48e2a08c6c3bec604f
Flora Harvey <fharvey555@gmail.com>
2017-03-07 13:04:25 +0000

    Set up controller and sinatra

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app/app.rb')
+
 require 'capybara'
 require 'capybara/rspec'
+require 'rspec'
+
+Capybara.app = BookmarkManager
 
 require './app/models/link'
 
