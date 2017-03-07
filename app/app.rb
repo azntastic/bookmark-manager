dc25ae2ab9d90b4340acb63a594854c2e687a939
Clem Capel-Bird <clemcapelbird@gmail.com>
2017-03-07 16:21:14 +0000

    User story 2 adding new links fulfilled. All tests passing

--- app/app.rb
+++ app/app.rb
@@ -9,7 +9,16 @@ class BookmarkManager < Sinatra::Base
 
   get '/links' do
     @links = Link.all
-    erb :links
+    erb :'links/index'
+  end
+
+  get '/links/new' do
+    erb :'links/new'
+  end
+
+  post '/links' do
+    Link.create(url: params[:url], title: params[:title])
+    redirect '/links'
   end
 
 end
---
+++ spec/features/creating_links_spec.rb
@@ -0,0 +1,14 @@
+# As a user
+# So that I can save a website I'm interested in
+# I would like to add the site's address and title to my bookmark manager
+
+feature "Save new link" do
+  scenario "User can save a new link to the bookmark manager" do
+    visit '/links'
+    click_button 'Add Link'
+    fill_in :title, with: 'Makers Academy'
+    fill_in :url, with: ('http://www.makersacademy.com/')
+    click_button 'Save Link'
+      expect(page).to have_content("Makers Academy")
+    end
+end
