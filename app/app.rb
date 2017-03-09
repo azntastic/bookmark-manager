16f5ea83b6292cf0ed4963e9b486c9fa970333ac
Flora Harvey <fharvey555@gmail.com>
2017-03-09 13:06:49 +0000

    Refactor datamapper config to separate file

--- app/app.rb
+++ app/app.rb
@@ -1,13 +1,12 @@
 require 'sinatra/base'
 require_relative 'models/link'
+require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
 
 ENV["RACK_ENV"] ||= 'development'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV["RACK_ENV"]}")
-DataMapper.finalize
-DataMapper.auto_upgrade!
+  data_mapper_config
 
   get '/' do
     'Hello World'
